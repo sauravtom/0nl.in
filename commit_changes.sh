a2912319cbb6798d5e42789822111d8b7a6fc30f
aakash-cr7 <aakash.tyg93@gmail.com>
2016-08-28 16:47:44 +0530

    Update shell file

--- commit_changes.sh
+++ commit_changes.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-echo "YOLO"
+
+git add --all
+if [ $1 == "add" ]
+then
+    git commit -m "Register user profile ${2}."
+elif [ $1 == "update ${2}" ] 
+then
+    git commit -m "Update user profile ${2}."
+fi
+git push origin master
